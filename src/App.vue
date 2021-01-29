d26cba6b1d03e91c0b2ea0b9743c1cc0b537806e
StarkShang <stark.shang@sjtu.edu.cn>
2021-01-29 20:19:43 +0800

    chore: 增加element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
     <div id="app">
-        Hello world
+        <div>
+            <el-button>el-button</el-button>
+        </div>
     </div>
 </template>
 
-<style lang="scss">
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+    name: "Application",
+})
+export default class Application extends Vue {}
+</script>
+
+<style>
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
